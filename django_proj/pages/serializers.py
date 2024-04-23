9e03af44e2663d76bd759ee41045d8cbd77626d6
Nagarajan-cloudside <160459859+Nagarajan-cloudside@users.noreply.github.com>
2024-04-23 13:03:37 +0530

    Delete django_proj directory

--- django_proj/pages/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-class BookSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        # table_name = "custom table name"
-        table_name = 'books'
-        fields = '__all__'
